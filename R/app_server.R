3dea5db35c4f424e3b5ccd9f16687ba8eafbb244
Jonah Thomas <73874835+jonahthomas@users.noreply.github.com>
2022-03-31 21:30:38 +0100

    add active_background_check to QAQC output table

--- R/app_server.R
+++ R/app_server.R
@@ -310,7 +310,8 @@ app_server <- function( input, output, session ) {
     sa_qaqc() %>%
       dplyr::mutate(
         length_check = dplyr::if_else(length_check == 1, as.character(shiny::icon("ok", lib = "glyphicon")), as.character(shiny::icon("ban-circle", lib = "glyphicon"))),
-        colname_check = dplyr::if_else(colname_check == 1, as.character(shiny::icon("ok", lib = "glyphicon")), as.character(shiny::icon("ban-circle", lib = "glyphicon")))
+        colname_check = dplyr::if_else(colname_check == 1, as.character(shiny::icon("ok", lib = "glyphicon")), as.character(shiny::icon("ban-circle", lib = "glyphicon"))),
+        active_background_check = dplyr::if_else(active_background_check == TRUE, as.character(shiny::icon("ok", lib = "glyphicon")), as.character(shiny::icon("ban-circle", lib = "glyphicon")))
       ),
     escape = FALSE, options = list(autoWidth = FALSE, scrollX = TRUE)
   )
